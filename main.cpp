0d5b059bb021fa4934e73290246ca96d0702115b
mehmet ali ipsuz <mehmetaliipsuz@hotmail.com>
2020-02-06 18:05:29 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,23 +2,43 @@
 #include "Movies.h"
 
 int main(){
-    int selection;
+    int selection {1};
     Movies MyFav;
 
-    std::cout << "What you want to do?" << std::endl;
-    std::cout << "Do you want to add new film? Press 1" << std::endl;
-    std::cout << "Do you want to revise existing film? Press 2" << std::endl;
+    while(selection >=1 & selection <3){
+        std::cout << "What you want to do?" << std::endl;
+        std::cout << "Do you want to add new film? Press 1" << std::endl;
+        std::cout << "To list all film, please press 2" << std::endl;
 
-    std::cin >> selection;
+        std::cin >> selection;
 
-    switch(selection){
-    case 1:
+        switch(selection){
+        case 1:
+            {
+            std::string movie_name;
+            int movie_rate;
+            int watch_count;
 
-        break;
-    case 2:
+            std::cout << "Type the movie name" << std::endl;
+            std::cin >> movie_name;
+            std::cout << "What is your rate for this movie? 0 to 10?" << std::endl;
+            std::cin >> movie_rate;
+            std::cout << "How many times have you watched this film?" << std::endl;
+            std::cin >> watch_count;
 
-        break;
+            MyFav.add_movie(movie_name, movie_rate, watch_count);
 
+            break;
+            }
+        case 2:
+            {
+                MyFav.get_movies();
+                break;
+            }
+        default:
+            std::cout << "Unexpected value!";
+            break;
+        }
     }
 
     return 0;
