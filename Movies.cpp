d9075a2bf4dfb0cc75662f2dba4d884c4b8b4eb4
mehmet ali ipsuz <mehmetaliipsuz@hotmail.com>
2020-02-06 18:04:35 -0600

    Update Movies.cpp

--- Movies.cpp
+++ Movies.cpp
@@ -1,5 +1,11 @@
 #include "Movies.h"
 
 void Movies::add_movie(std::string name, int rate, int watch_count){
-
+    Movie new_movie(name,rate,watch_count);
+    v_Movies.push_back(new_movie);
+}
+void Movies::get_movies(){
+    for(Movie temp_mov : v_Movies){
+        std::cout << temp_mov.get_movie_name() << std::endl;
+    }
 }
