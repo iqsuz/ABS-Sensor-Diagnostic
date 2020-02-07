14f424c7f37d09e6ec77aabd59c23c29e3033220
mehmet ali ipsuz <mehmetaliipsuz@hotmail.com>
2020-02-06 18:04:13 -0600

    Update Movie.h

--- Movie.h
+++ Movie.h
@@ -6,13 +6,14 @@
 
 class Movie{
     private:
-        std::string m_movie_name;
-        int m_rate;
-        int m_watch_count;
+        std::string c_movie_name;
+        int c_rate;
+        int c_watch_count;
     public:
-        Movie(std::string m_name, int m_rate, int watch_count);
-        void set_movie_name(std::string m_name);
-        void set_rate(int rate);
+        Movie(std::string movie_name, int movie_rate = 0, int watch_count = 0);
+        Movie();
+        void set_movie_name(std::string movie_name);
+        void set_rate(int movie_rate);
         void set_watch_count(int watch_count);
         std::string get_movie_name();
         int get_rate();
