4a3d792a2cebe072df4fda97a5f6a79ecb5237a3
mehmet ali ipsuz <mehmetaliipsuz@hotmail.com>
2020-02-06 18:03:37 -0600

    Update Movie.cpp

--- Movie.cpp
+++ Movie.cpp
@@ -1,29 +1,30 @@
 #include "Movie.h"
 
-Movie::Movie(std::string m_name, int m_rate, int watch_count){
-    m_movie_name = m_name;
-    m_rate =
+Movie::Movie(std::string movie_name, int movie_rate, int watch_count){
+    c_movie_name = movie_name;
+    c_rate = movie_rate;
+    c_watch_count = watch_count;
 }
 
-void Movie::set_movie_name(std::string m_name){
-    m_movie_name = m_name;
+void Movie::set_movie_name(std::string movie_name){
+    c_movie_name = movie_name;
 }
-void Movie::set_rate(int rate){
-    m_rate = rate;
+void Movie::set_rate(int movie_rate){
+    c_rate = movie_rate;
 }
 
 void Movie::set_watch_count(int watch_count){
-    m_watch_count = watch_count;
+    c_watch_count = watch_count;
 }
 
 std::string Movie::get_movie_name(){
-    return m_movie_name;
+    return c_movie_name;
 }
 
 int Movie::get_rate(){
-    return m_rate;
+    return c_rate;
 }
 
 int Movie::get_watch_count(){
-    return m_watch_count;
+    return c_watch_count;
 }
